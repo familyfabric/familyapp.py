2bde1cb357c28a4a00d57bf29e483a8a1237ef05
Piotr Giedziun <piotrgiedziun@gmail.com>
2017-09-05 10:44:42 +0200

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(name='familyapp.py',
       author='Piotr Giedziun',
-      version='0.0.3',
+      version='0.0.4',
       packages=['familyapp',],
       license='MIT',
       author_email='piotrgiedziun@gmail.com',

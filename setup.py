3e3ee878ac311eb74bb3f5d7321966ac011420d3
Piotr Giedziun <piotrgiedziun@Piotrs-iMac.local>
2017-11-22 11:24:21 +0100

    update version to 0.0.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(name='familyapp.py',
       author='Piotr Giedziun',
-      version='0.0.8',
+      version='0.0.9',
       packages=['familyapp',],
       license='MIT',
       author_email='piotrgiedziun@gmail.com',
